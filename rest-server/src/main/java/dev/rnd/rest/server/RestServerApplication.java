66e4e1a49a6dab1810d14e2b9a944c2df6ac0451
asador <reza.a@pointclickcare.com>
2022-09-30 14:53:47 -0400

    Check in rest server skeleton

---
+++ rest-server/src/main/java/dev/rnd/rest/server/RestServerApplication.java
@@ -0,0 +1,13 @@
+package dev.rnd.rest.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServerApplication.class, args);
+	}
+
+}
